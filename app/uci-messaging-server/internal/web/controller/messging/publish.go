98839ec1b3f4953f99ec3d2273473cf61e296b81
hekangning <hekangning@coding.net>
2022-04-25 14:14:16 +0800

    messaging web

---
+++ app/uci-messaging-server/internal/web/controller/messging/publish.go
@@ -0,0 +1,17 @@
+package messging
+
+import (
+	"github.com/cheerego/uci/app/uci-messaging-server/internal/shim/watcher"
+	"github.com/labstack/echo/v4"
+)
+
+func Publish(c echo.Context) error {
+	payload := c.QueryParam("payload")
+	name := c.Param("name")
+	if ch, load := watcher.Load(name); load {
+		ch.(chan string) <- payload
+		return c.String(200, "ok")
+	} else {
+		return c.String(200, "no subscribe")
+	}
+}
