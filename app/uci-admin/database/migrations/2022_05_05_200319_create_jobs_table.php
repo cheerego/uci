d2a74a0a82d17f2f7e2316618e622fbda374d0a5
hekangning <hekangning@coding.net>
2022-05-05 20:12:14 +0800

    添加 laravel admin

---
+++ app/uci-admin/database/migrations/2022_05_05_200319_create_jobs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->integer("pipeline_id");
+            $table->integer("trigger_user_id");
+            $table->integer("trigger_message");
+            $table->string("status");
+            $table->text("content");
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
