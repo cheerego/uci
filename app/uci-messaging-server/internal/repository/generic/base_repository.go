cbb87e5e7cf1c4f7a4c60a6e0d4d1ac44b76f8e2
cheerego <41974873@qq.com>
2022-05-16 00:32:07 +0800

    完善数据库泛型

--- app/uci-messaging-server/internal/repository/generic/base_repository.go
+++ app/uci-messaging-server/internal/repository/generic/base_repository.go
@@ -14,9 +14,71 @@ func NewBaseRepository[T any]() *BaseRepository[T] {
 
 func (b *BaseRepository[T]) FindById(ctx context.Context, id int32) (*T, error) {
 	var m T
-	err := storage.FromContext(ctx).Model(m).First(&m, id).Error
+	err := storage.FromContext(ctx).First(&m, id).Error
 	if err != nil {
 		return nil, err
 	}
 	return &m, err
+}
+
+func (b *BaseRepository[T]) FindByIdWithDeleted(ctx context.Context, id int32) (*T, error) {
+	var m T
+	err := storage.FromContext(ctx).Unscoped().First(&m, id).Error
+	if err != nil {
+		return nil, err
+	}
+	return &m, err
+}
+
+func (b *BaseRepository[T]) FindByIds(ctx context.Context, id ...int32) ([]*T, error) {
+	m := make([]*T, 0)
+
+	err := storage.FromContext(ctx).Find(&m, id).Error
+	if err != nil {
+		return nil, err
+	}
+	return m, err
+}
+
+func (b *BaseRepository[T]) FindByIdsWithDeleted(ctx context.Context, id ...int32) ([]*T, error) {
+	m := make([]*T, 0)
+	err := storage.FromContext(ctx).Unscoped().Find(&m, id).Error
+	if err != nil {
+		return nil, err
+	}
+	return m, err
+}
+
+func (b *BaseRepository[T]) Insert(ctx context.Context, m *T) error {
+	err := storage.FromContext(ctx).Create(m).Error
+	return err
+}
+
+func (b *BaseRepository[T]) BatchInsert(ctx context.Context, m []*T) (int64, error) {
+	tx := storage.FromContext(ctx).Create(m)
+	return tx.RowsAffected, tx.Error
+}
+
+func (b *BaseRepository[T]) DeleteById(ctx context.Context, id int32) (int64, error) {
+	var m T
+	tx := storage.FromContext(ctx).Delete(&m, id)
+	return tx.RowsAffected, tx.Error
+}
+
+func (b *BaseRepository[T]) DeleteByIds(ctx context.Context, ids int32) (int64, error) {
+	var m T
+	tx := storage.FromContext(ctx).Delete(&m, ids)
+	return tx.RowsAffected, tx.Error
+}
+
+func (b *BaseRepository[T]) ForceDeleteById(ctx context.Context, id int32) (int64, error) {
+	var m T
+	tx := storage.FromContext(ctx).Unscoped().Delete(&m, id)
+	return tx.RowsAffected, tx.Error
+}
+
+func (b *BaseRepository[T]) ForceDeleteByIds(ctx context.Context, ids ...int32) (int64, error) {
+	var m T
+	tx := storage.FromContext(ctx).Unscoped().Delete(&m, ids)
+	return tx.RowsAffected, tx.Error
 }
