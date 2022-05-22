15f614004b286969393bb952ddc69ff3db0bc5ca
cheerego <41974873@qq.com>
2022-05-22 23:04:24 +0800

    完善 watch 代码

--- app/cli/root.go
+++ app/cli/root.go
@@ -1,13 +1,11 @@
 package cli
 
 import (
-	"fmt"
-	"github.com/cheerego/uci/app/cli/internal/cmd"
+	"github.com/cheerego/uci/app/cli/internal/executor"
+	"github.com/cheerego/uci/app/cli/internal/executor/shim"
 	"github.com/cheerego/uci/pkg/log"
 	"github.com/mitchellh/go-homedir"
-	"github.com/spf13/cobra"
 	"go.uber.org/zap"
-	"os"
 	"path"
 )
 
@@ -20,22 +18,6 @@ func init() {
 	zap.ReplaceGlobals(cli)
 }
 
-func init() {
-	rootCmd.AddCommand(cmd.UpCmd)
-}
-
-var rootCmd = &cobra.Command{
-	Use:   "executor",
-	Short: "executor is a simple CI execute engine",
-	Long:  `executor is a simple CI execute engine, message communicate by List And Watch、Websocket、RabbitMQ`,
-	Run: func(cmd *cobra.Command, args []string) {
-		cmd.Help()
-	},
-}
-
 func Execute() {
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	executor.NewUci(shim.NewListWatch()).Root().Execute()
 }
