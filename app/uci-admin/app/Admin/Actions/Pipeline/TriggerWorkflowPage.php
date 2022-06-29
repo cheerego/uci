4675f491fe1926a3df89da9f32016f35d003f050
cheerego <41974873@qq.com>
2022-06-29 14:04:19 +0800

    添加很多代码

--- app/uci-admin/app/Admin/Actions/Pipeline/TriggerWorkflowPage.php
+++ app/uci-admin/app/Admin/Actions/Pipeline/TriggerWorkflowPage.php
@@ -3,6 +3,7 @@
 namespace App\Admin\Actions\Pipeline;
 
 use App\Models\Workflow;
+use App\Services\WorkflowService;
 use Encore\Admin\Actions\RowAction;
 
 class TriggerWorkflowPage extends RowAction
@@ -11,11 +12,33 @@ class TriggerWorkflowPage extends RowAction
 
     public function handle(Workflow $model)
     {
-        return $this->response()->redirect(route(admin_get_route("workflows.trigger.page"),[$model->id]));
+        $workflowService = app(WorkflowService::class);
+
+        $revision = request()->input("revision");
+//        $param_envs = $request->input("envs", []);
+//        $needle_param_envs = [];
+//        foreach ($param_envs as $key => $value) {
+//            $needle_param_envs[] = $value;
+//        }
+        $params = [
+            "revision" => $revision,
+//            "envs" => $needle_param_envs
+        ];
+        $resp = $workflowService->triggerBuild($model->id, $params);
+        if ($resp->getStatusCode() != 200) {
+            return $this->response()->error("触发构建失败" . $resp->getBody())->refresh();
+        }
+        return $this->response()->success("触发构建成功")->refresh();
     }
+
     public function form()
     {
-        $this->text('file', '请选择文件');
+        $this->text("revision", __("Revision"));
+//        $this->text("envs",)
+//        $this->table('envs', __('Param envs'), function ($table) {
+//            $table->text('key');
+//            $table->text('value');
+//        });
     }
 
 }
